60ecd348f3318bfa11e24ab887f35c3e3217587d
mohammadhoseindolati <90058570+mohammadhoseindolati@users.noreply.github.com>
2023-03-09 13:41:33 +0330

    add Model&Controller Brand

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/admin-panel/dashboard', function () {
     return view('admin.dashboard');
+})->name('dashboard');
+
+Route::prefix('admin-panel/management')->name('admin.')->group(function (){
+    Route::resource('brands' , \App\Http\Controllers\Admin\BrandController::class) ;
 });
