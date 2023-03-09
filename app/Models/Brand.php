bcc93566f0bc0bb0733518a4369e3b7092094d92
mohammadhoseindolati <90058570+mohammadhoseindolati@users.noreply.github.com>
2023-03-09 14:00:20 +0330

    Add Sluggable Package and config to support Persian

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -4,11 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Brand extends Model
 {
-    use HasFactory;
+    use HasFactory,Sluggable;
 
     protected $table = "brands" ;
     protected $guarded = [] ;
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
 }
