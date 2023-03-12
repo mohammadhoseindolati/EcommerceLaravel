a8d54baa2225bc0fcf69fd581e43520904fb1e70
mohammadhoseindolati <90058570+mohammadhoseindolati@users.noreply.github.com>
2023-03-12 15:37:08 +0330

    create brand form

---
+++ resources/views/admin/brands/create.blade.php
@@ -0,0 +1,38 @@
+@extends('admin.layouts.admin')
+
+@section('content')
+
+    <!-- Content Row -->
+    <div class="row">
+
+        <div class="col-xl-12 col-md-12 mb-4 p-md-5 bg-white">
+           <div class="mb-4">
+               <h5 class="font-weight-bold">ایجاد برند </h5>
+           </div>
+            <hr>
+
+                <form action="{{ route('admin.brands.store') }}" method="post">
+                    @csrf
+                    <div class="form-row">
+
+                    <div class="form-group col-md-3">
+                        <label for="name">نام</label>
+                        <input type="text" class="form-control" name="name" id="name">
+                    </div>
+
+                    <div class="form-group col-md-3">
+                        <label for="is_active">وضعیت</label>
+                        <select class="form-control" name="is_active" id="is_active">
+                            <option value="1" selected>فعال</option>
+                            <option value="2">غیر فعال</option>
+                        </select>
+                    </div>
+                    </div>
+
+                    <button class="btn btn-outline-primary mt-5" type="submit">ثبت </button>
+                    <a href="{{ route('admin.brands.index') }}" class="btn btn-dark mt-5 mr-3">بازگشت</a>
+
+                </form>
+        </div>
+    </div>
+@endsection
