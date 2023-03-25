2eca88c176e1b4cb54af1896effb1e7676a067e9
mohammadhoseindolati <90058570+mohammadhoseindolati@users.noreply.github.com>
2023-03-25 11:37:51 +0430

    fix: column parent_id in category

--- database/migrations/2023_02_04_103851_create_categories_table.php
+++ database/migrations/2023_02_04_103851_create_categories_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
 
-            $table->foreignId('parent_id')->constrained();
+            $table->unsignedInteger('parent_id')->default(0);
 
             $table->string('name') ;
             $table->string('slug')->unique()->nullable() ;
